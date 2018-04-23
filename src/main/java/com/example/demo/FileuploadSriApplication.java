fe704a398a9c3456c335a916cc443a44df8c46b4
Srmanjun723 <33734774+Srmanjun723@users.noreply.github.com>
2018-04-23 00:17:06 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/FileuploadSriApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadSriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadSriApplication.class, args);
+	}
+}
